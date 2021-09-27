491212acb8f413d36ca392d177ee00de2fa2d159
Hatice <haticekaraty@gmail.com>
2021-09-27 14:32:01 -0400

    Done.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import shoppingListItemReducer from './reducers/shoppingListItemReducer';
+import { createStore } from 'redux';
+
+const store = createStore(
+  shoppingListItemReducer, 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Provider store={store}>
+    <App />
+  </Provider>, div);
 });
